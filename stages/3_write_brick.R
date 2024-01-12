3b5169cd9dd4b7f28ce97e4bddd63ed10d906443
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-01-12 09:36:27 -0500

    Convert more PubChem IDs to string type
    instead of double type.

--- stages/3_write_brick.R
+++ stages/3_write_brick.R
@@ -3,6 +3,9 @@ library(tidyverse)
 
 # tox21lib with reference data on substances
 tox21lib <- readr::read_tsv("raw/tox21_10k_library_info.tsv/tox21_10k_library_info.tsv")
+tox21lib <- tox21lib %>%
+  mutate(PUBCHEM_CID = as.character(as.integer(PUBCHEM_CID)),
+         PUBCHEM_SID = as.character(as.integer(PUBCHEM_SID)))
 arrow::write_parquet(tox21lib,"brick/tox21lib.parquet")
 
 # aggregated files
@@ -11,6 +14,9 @@ aggtable <- map(aggfiles,~readr::read_tsv(.x))
 aggtable <- keep(aggtable,~nrow(.x)>0)
 aggtable <- map(aggtable, ~ mutate(., SAMPLE_DATA_TYPE = as.character(SAMPLE_DATA_TYPE)))
 aggmerge <- bind_rows(aggtable)
+aggmerge <- aggmerge %>%
+  mutate(PUBCHEM_CID = as.character(as.integer(PUBCHEM_CID)),
+         PUBCHEM_SID = as.character(as.integer(PUBCHEM_SID)))
 arrow::write_parquet(aggmerge,"brick/tox21_aggregated.parquet")
 
 # raw files
